4607775069f4a34608914f3fbda941d40b23b3cf
foamy1881 <fongyang@hotmail.com>
2017-09-23 16:05:22 +1200

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,27 @@
+# Exploratory Data Analysis - Assignment 2
+# Question 2
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+# Yang Fong September 20, 2017
+# Source code to plot2.png
+
+library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+                    , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
+png(filename='plot2.png')
+
+barplot(totalNEI[, Emissions]
+          , names = totalNEI[, year]
+          , xlab = "Years", ylab = "Emissions"
+          , main = "Emissions over the Years")
+          
+          dev.off() 
